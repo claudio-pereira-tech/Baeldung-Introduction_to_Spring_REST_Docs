acbba8b1873ac022c2139b5ddc5854cd6a76c1f8
Cláudio de Oliveira Pereira <claudio@pereira.tech>
2024-08-25 20:20:34 -0700

    Update Gradle build project

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.3.2"
+	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
 	id("org.asciidoctor.jvm.convert") version "4.0.3"
 }
@@ -19,15 +19,18 @@ repositories {
 }
 
 extra["snippetsDir"] = file("build/generated-snippets")
+val asciidoctorExtensions: Configuration by configurations.creating
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-hateoas:3.3.2")
-	implementation("org.springframework.boot:spring-boot-starter-validation:3.3.2")
-	implementation("org.springframework.boot:spring-boot-starter-web:3.3.2")
+	asciidoctorExtensions("org.springframework.restdocs:spring-restdocs-asciidoctor:3.0.1")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.2")
+	implementation("org.springframework.boot:spring-boot-starter-hateoas:3.3.3")
+	implementation("org.springframework.boot:spring-boot-starter-validation:3.3.3")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.3.3")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.3")
 	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc:3.0.1")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.3")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.11.0")
 }
 
 tasks.withType<Test> {
@@ -40,5 +43,6 @@ tasks.test {
 
 tasks.asciidoctor {
 	inputs.dir(project.extra["snippetsDir"]!!)
+	configurations("asciidoctorExtensions")
 	dependsOn(tasks.test)
 }
